942b9f76f2460a560af444cfeb290349f6038096
Nils Matic <matic1909@gmail.com>
2023-05-10 17:03:32 +0200

    added input to search for a city and functin to render fetched data on the screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const mainContainer = document.querySelector("main");
+const weatherDisplay = document.querySelector(".weather-display");
+const searchInput = document.getElementById("city-search");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", (e) => {
+  const searchTerm = searchInput.value;
+  if (searchTerm) {
+    renderWeatherInfo(searchTerm);
+  } else {
+    console.log("Please enter a city!");
+  }
+});
+
 async function getWeatherData(location) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=3f6e7ee7894f4445b1a90741231005&q=${location}`,
@@ -7,4 +21,22 @@ async function getWeatherData(location) {
   );
   const data = await response.json();
   return data;
+}
+
+async function processWeatherData(location) {
+  const weatherData = await getWeatherData(location);
+  const weatherObject = {
+    location,
+    temp: weatherData.current.temp_c,
+    condition: weatherData.current.condition.text,
+  };
+  return weatherObject;
+}
+
+async function renderWeatherInfo(location) {
+  weatherDisplay.innerHTML = "";
+  const weather = await processWeatherData(location);
+  const weatherDescription = document.createElement("p");
+  weatherDescription.textContent = `Current weather in ${location}: ${weather.temp} degrees Celsius, ${weather.condition}`;
+  weatherDisplay.appendChild(weatherDescription);
 }
