e4d0122a28d24c6be3ab7edc15a8b68fca59739e
Nils Matic <matic1909@gmail.com>
2023-11-27 15:12:05 +0100

    added function for processing and printing the data and search input

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const getWeatherInfo = async () => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Osaka&units=metric&appid=56400427bd7789056ad727ca690b793d`);
-  const data = await response.json();
-  
-  console.log(`The weather in ${data.name}: It is currently ${data.main.temp}°C.`);
+const getWeatherInfo = async (location) => {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=56400427bd7789056ad727ca690b793d`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-getWeatherInfo();
+const processWeatherData = async (location) => {
+  const data = await getWeatherInfo(location);
+  return { name: data.name, temperature: data.main.temp, weather: data.weather[0].description };
+}
+
+const printWeatherData = async (location) => {
+  const weather = await processWeatherData(location);
+  console.log(weather);
+}
+
+const locationInput = document.getElementById("location");
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  printWeatherData(locationInput.value);
+  locationInput.value = "";
+})
